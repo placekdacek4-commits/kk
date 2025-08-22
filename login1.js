609fe61c5da313898f2b1139c81bd8eed7b47b15
placekdacek4-commits <placekdacek4@gmail.com>
2025-08-22 19:25:00 +0200

    Update login1.js

--- login1.js
+++ login1.js
@@ -17,7 +17,7 @@ function sprawdzDane() {
     var haslo = document.getElementById("haslo").value;
 
     var users = {
-        "Qx_2!n$Vr@k#Tm": "Zp_5@b$Hq!n#Fx",
+        "adam": "widurski",
     };
 
     // Weryfikacja danych
